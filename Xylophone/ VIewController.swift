27e878404daf447740009b6409315f3a821f6207
Tanja Hennis <tmhennis@gmail.com>
2018-04-05 16:41:00 +0200

    keys play different notes

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-
+    var audioPlayer: AVAudioPlayer!
+    var selectedSoundFileName : String = ""
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
-
+ 
     @IBAction func notePressed(_ sender: UIButton) {
+        selectedSoundFileName = soundArray[sender.tag - 1]
         
-        
-        
+        print(selectedSoundFileName)
+        playSound()
     }
     
-  
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
 
 }
-
